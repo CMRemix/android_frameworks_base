b88a0ec78d73584015957977d18534c737bea2c7
siracuervo <fl.siracusa@outlook.com.ar>
2015-05-30 20:38:46 -0700

    (SQUASH) Power menu button in expanded statusbar
    Based on @rascarlo and boostpop work...
    
    Rogersb11: FIX the layout so it isn't jacked lol and make icon respect custom user header color
    
    Change-Id: I2b8014725544c068e4fb4453d33cf3049cd9c741
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/layout/status_bar_expanded_header.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4170,6 +4170,15 @@ public final class Settings {
         public static final String PIE_TRIGGER_SENSITIVITY = "pie_trigger_sensitivity";
 
         /**
+         * Status bar power menu
+         * 0 - disabled (default)
+         * 1 - screen off / power menu
+         * 2 - power menu - screen off
+         * @hide
+         */
+        public static final String STATUS_BAR_POWER_MENU = "status_bar_power_menu";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
