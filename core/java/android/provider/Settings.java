daea0b7bf3ba255d45bda45e75d0796116fb928c
rogersb11 <brettrogers11@gmail.com>
2015-06-25 20:05:02 -0300

    Slim actions floating mode swtich 1/2
    Change-Id: Id111fc5eef4c7e653fbbbdaf5c1e577cb42395e7

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4914,6 +4914,14 @@ public final class Settings {
          */
         public static final String ENABLE_NAVIGATION_RING = "enable_navigation_ring";
 
+
+        /**
+         * Launch apps in floating window or not
+         *
+         * @hide
+         */
+        public static final String SLIM_ACTIONS_USE_FLOATING_WINDOW = "slim_action_use_floating_window";
+        
         /**
          * @hide
          */
