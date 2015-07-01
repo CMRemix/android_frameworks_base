334b3d6d528dc81a26f70ee92457bd8de13a9223
rogersb11 <brettrogers11@gmail.com>
2015-07-01 01:05:08 -0700

    Slim action float tile 2/2
    Credits to BlackDragon for the drawables
    Change-Id: I3a0928c81ab1d66332057755b360302b570f8c61
    
    Conflicts:
    	packages/SystemUI/res/values/strings_aicp.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

--- core/java/com/android/internal/util/cm/QSConstants.java
+++ core/java/com/android/internal/util/cm/QSConstants.java
@@ -65,6 +65,7 @@ public class QSConstants {
     public static final String TILE_APPSIDEBAR = "toggleAppSideBar";
     public static final String TILE_HWKEYS = "hwkeys";
     public static final String TILE_SYSTEMUI_RESTART = "reboot_systemui";
+    public static final String TILE_SLIM_FLOATS = "slim_floats";
 
     public static final String DYNAMIC_TILE_NEXT_ALARM = "next_alarm";
     public static final String DYNAMIC_TILE_IME_SELECTOR = "ime_selector";
@@ -120,6 +121,7 @@ public class QSConstants {
         STATIC_TILES_AVAILABLE.add(TILE_APPSIDEBAR);
         STATIC_TILES_AVAILABLE.add(TILE_HWKEYS);
         STATIC_TILES_AVAILABLE.add(TILE_SYSTEMUI_RESTART);
+        STATIC_TILES_AVAILABLE.add(TILE_SLIM_FLOATS);
 
         DYNAMIC_TILES_AVAILABLE.add(DYNAMIC_TILE_NEXT_ALARM);
         DYNAMIC_TILES_AVAILABLE.add(DYNAMIC_TILE_IME_SELECTOR);
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -75,6 +75,7 @@ import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.AppCircleBarTile;
 import com.android.systemui.qs.tiles.UsbTetherTile;
+import com.android.systemui.qs.tiles.SlimFloatsTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.qs.tiles.TrdsTile;
 import com.android.systemui.qs.tiles.AppSideBarTile;
@@ -417,6 +418,8 @@ public class QSTileHost implements QSTile.Host {
                 return new HWKeysTile(this);
             case QSConstants.TILE_SYSTEMUI_RESTART:
                 return new SystemUIRestartTile(this);
+            case QSConstants.TILE_SLIM_FLOATS:
+                return new SlimFloatsTile(this);
             default:
                 throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
         }
