97dad077a9d1fe62e83df54c7c3266485e44b5ec
Roman Birg <roman@cyngn.com>
2015-07-17 15:18:28 -0700

    SystemUI: attach a queue to media player notifications
    Extend media player notificatoins to attach a queue below them, set via
    the MediaController.setQueue() API, when available.
    
    Long pressing on the notificaiton will reveal an option to disable the
    queue.
    
    The queue is currently hardcoded to be limited to 3 items.
    
    REF: CYNGNOS-217
    Change-Id: I3b8503cb47af2d36f031d06aefc1bfbe7ed80010
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -2164,6 +2164,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     }
 
     public void updateSettings() {
+        Slog.i(TAG, "+++++ updateSettings() mUserRotationMode; " + mUserRotationMode + ", mUserRotation: " + mUserRotation);
         ContentResolver resolver = mContext.getContentResolver();
         boolean updateRotation = false;
         int mDeviceHardwareWakeKeys = mContext.getResources().getInteger(
@@ -2355,6 +2356,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
             WindowManagerPolicyControl.reloadFromSetting(mContext);
         }
+        Slog.i(TAG, "----- updateSettings() mUserRotationMode; " + mUserRotationMode + ", mUserRotation: " + mUserRotation + "updateRotation: " + updateRotation);
         if (updateRotation) {
             updateRotation(true);
         }
@@ -6845,7 +6847,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     @Override
     public int rotationForOrientationLw(int orientation, int lastRotation) {
-        if (false) {
+        if (true) {
             Slog.v(TAG, "rotationForOrientationLw(orient="
                         + orientation + ", last=" + lastRotation
                         + "); user=" + mUserRotation + " "
@@ -7059,6 +7061,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     // User rotation: to be used when all else fails in assigning an orientation to the device
     @Override
     public void setUserRotationMode(int mode, int rot) {
+        Slog.i(TAG, "setUserRotationMode() called with " + "mode = [" + mode + "], rot = [" + rot + "]");
         ContentResolver res = mContext.getContentResolver();
 
         // mUserRotationMode and mUserRotation will be assigned by the content observer
