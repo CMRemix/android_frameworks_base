8b1e73cab797aa1ea96cc9851d445db606d9db45
Randall Rushing <randall.rushing@gmail.com>
2015-08-15 17:38:11 -0700

    QSTiles: Set number of tile columns (1 of 2)
    3, 4, or 5 columns. We don't want to get excessive here
    
    We keep the Quick Settings panel clean by scaling some
    dimens using a factor determined by the column count
    
    In QSPanel, we scale:
      -- mCellHeight
      -- mCellWidth
      -- and add mColumns as a member
    
    in QSTileView, we scale:
      -- mIconSizePx
      -- mTilePaddingBelowIconPx
      -- R.dimen.qs_tile_text_size (dynamically generated)
      -- and add mSizeScale as a member
    
    Change-Id: I6fc8652ce3fe41521958de04a86e59a15f9f1445
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/values/bliss_attrs.xml
    	packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -658,6 +658,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_WEATHER_SIZE),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.Secure.getUriFor(
+                    Settings.Secure.QS_NUM_TILE_COLUMNS), false, this,
+                    UserHandle.USER_ALL);
             update();
         }
 
@@ -884,6 +887,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     1000, mCurrentUserId);
             if (mShowLabelTimeout < 100) {
                 mShowLabelTimeout = 100;
+            }
+
+            if (mQSPanel != null) {
+                mQSPanel.updateNumColumns();
             }
         }
     }
