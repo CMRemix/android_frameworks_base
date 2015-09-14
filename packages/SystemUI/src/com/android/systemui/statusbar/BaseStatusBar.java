8ea476afefbf79d1669d6db508ac939c3ee80e52
Adnan Begovic <adnan@cyngn.com>
2015-09-14 10:56:03 -0700

    SystemUi: Prioritize call notifications which interrupt.
      Posting notifications to the front of the message queue in the
      SystemUi handler can save 800ms -> 1.5s of real time when receiving
      and call notifications under heavy load.
    
    TICKET: CYNGNOS-969
    Change-Id: I56e4a9482e8f8cc8fed97cdb4a46625ddddcf277
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -569,7 +569,7 @@ public abstract class BaseStatusBar extends SystemUI implements
         public void onNotificationPosted(final StatusBarNotification sbn,
                 final RankingMap rankingMap) {
             if (DEBUG) Log.d(TAG, "onNotificationPosted: " + sbn);
-            mHandler.post(new Runnable() {
+            final Runnable notificationPost = new Runnable() {
                 @Override
                 public void run() {
                     Notification n = sbn.getNotification();
@@ -599,7 +599,12 @@ public abstract class BaseStatusBar extends SystemUI implements
                         addNotification(sbn, rankingMap);
                     }
                 }
-            });
+            };
+            if (TextUtils.equals(sbn.getNotification().category, Notification.CATEGORY_CALL)) {
+                mHandler.postAtFrontOfQueue(notificationPost);
+            } else {
+                mHandler.post(notificationPost);
+            }
         }
 
         @Override
@@ -1510,7 +1515,7 @@ public abstract class BaseStatusBar extends SystemUI implements
     public abstract void resetHeadsUpDecayTimer();
     public abstract void hideHeadsUp();
 
-    public abstract void scheduleHeadsUpOpen();
+    public abstract void scheduleHeadsUpOpen(boolean immediate);
 
     public abstract void scheduleHeadsUpClose();
 
