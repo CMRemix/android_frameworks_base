c9c1c8643b12e45380a414778bbac8513c16e9fd
rogersb11 <brettrogesr11@gmail.com>
2015-09-17 00:36:04 -0700

    QS: Slim Pie Control tile (1/2)
    PS2: New pref name
    PS3: Use get/putIntForUser
    PS4: Finish fixing user settings
    
    pa pie tile >> slim pie tile
    
    Change-Id: I1904913adfc08d197a9ba4a802b9ebfd2f4f09ac
    Signed-off-by: rogersb11 <brettrogesr11@gmail.com>
    
    Conflicts:
    	core/java/com/android/internal/util/cm/QSConstants.java
    	packages/SystemUI/res/values/custom_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -57,6 +57,7 @@ import com.android.systemui.qs.tiles.LockscreenToggleTile;
 import com.android.systemui.qs.tiles.LteTile;
 import com.android.systemui.qs.tiles.MusicTile;
 import com.android.systemui.qs.tiles.NotificationsTile;
+import com.android.systemui.qs.tiles.PieControlTile;
 import com.android.systemui.qs.tiles.ProfilesTile;
 import com.android.systemui.qs.tiles.PerfProfileTile;
 import com.android.systemui.qs.tiles.PowerMenuTile;
@@ -429,6 +430,8 @@ public class QSTileHost implements QSTile.Host {
                 return new ThemesTile(this);
             case QSConstants.TILE_KERNEL_ADIUTOR:
                 return new KernelAdiutorTile(this);
+            case QSConstants.TILE_SLIM_PIE:
+                return new PieControlTile(this);
             default:
                 throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
         }
