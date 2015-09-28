f0a941551063b9e1eb9cbc27daf2da77a25972bc
ZION959 <ziontran@gmail.com>
2015-09-28 10:58:19 -0700

    revert Fixed: Wrong status bar notification layout width.
    The layout of the status bar which surrounds the notification icons
    should be a multiple of the notification icon width. Previous calculation
    ensured to be a multiple of the notification icon height.
    
    Change-Id: I35faf50b75f7521116065568b9f9142f16fbae2d (reverted from commit 2eb54db8f67dec606e4f701a641bf3150e64b3f0)

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1392,7 +1392,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         updateDisplaySize(); // populates mDisplayMetrics
         updateResources(null);
 
-        mIconSize = res.getDimensionPixelSize(R.dimen.status_bar_icon_size);
+        mIconSize = res.getDimensionPixelSize(com.android.internal.R.dimen.status_bar_icon_size);
 
         mStatusBarWindowContent = (FrameLayout) View.inflate(context,
                 R.layout.super_status_bar, null);
@@ -2724,9 +2724,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private void updateNotificationIcons() {
         if (mNotificationIcons == null) return;
 
-        final int effectiveIconWidth = IconMerger.calculateIconWidth(mContext);
         final LinearLayout.LayoutParams params
-            = new LinearLayout.LayoutParams(effectiveIconWidth, mNaturalBarHeight);
+            = new LinearLayout.LayoutParams(mIconSize + 2*mIconHPadding, mNaturalBarHeight);
 
         ArrayList<Entry> activeNotifications = mNotificationData.getActiveNotifications();
         final int N = activeNotifications.size();
@@ -5107,7 +5106,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 com.android.internal.R.dimen.status_bar_height);
 
         int newIconSize = res.getDimensionPixelSize(
-            R.dimen.status_bar_icon_size);
+            com.android.internal.R.dimen.status_bar_icon_size);
         int newIconHPadding = res.getDimensionPixelSize(
             R.dimen.status_bar_icon_padding);
 
