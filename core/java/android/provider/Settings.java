f06329ed6cce17518998a4786cd8f01dcd656623
ZION959 <ziontran@gmail.com>
2015-09-28 21:29:31 -0700

    Remove current built in add blocker.
    Change-Id: Icc5122623dc9be3038931657278ff77ffcdd14c9

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10832,13 +10832,6 @@ public final class Settings {
         public static final String VOLUME_STEPS_VOICE_CALL = "volume_steps_voice_call";
 
         /**
-         * Base: Implement ad blocker [2/3]
-	 * Disable ads (HFM)
-	 * @hide
-	 */
-	public static final String HFM_DISABLE_ADS = "hfm_disable_ads";
-
-        /**
          * volume answer.
          * @hide
          */
