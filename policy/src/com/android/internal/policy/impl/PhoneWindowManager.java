896aa6d075bb3cd6d510aa8df9bf22107ffb4a6c
ZION959 <ziontran@gmail.com>
2015-11-03 09:43:29 -0700

    CMRemix Lollipop 5.1.1_R26
    Signed-off-by: ZION959 <ziontran@gmail.com>

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -7203,7 +7203,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     mBootMsgDialog.show();
                 }
                 mBootMsgDialog.setMessage("Powered By SaberMod Optimizations\n\n" + msg
-                + "\n\nCMRemix Lollipop 5.1.1_R24");
+                + "\n\nCMRemix Lollipop 5.1.1_R26");
             }
         });
     }
