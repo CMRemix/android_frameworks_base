51880da273fc38f3159fe8b5c66d43b7bc5036f9
Squadzone <ai.the.smarties.physics@gmail.com>
2015-12-27 02:50:35 -0700

    Implement App circle sidebar [1/2]
    PS#2  Support for Configurable Trigger Region
    Change-Id: I16c15a158a127c0c6201c2d01b76a8885098024b
    
    PS#3  Fix FC when killing apps with app circlebar
    Change-Id: I83c3ffb09e1f7ca360129b7fcc3a349eb8d456c8
    
    Signed-off-by: Janson Kang <temasek71@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/values/cm_dimens.xml

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1118,6 +1118,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         addGestureAnywhereView();
         addSidebarView();
+        addAppCircleSidebar();
 
         if (mNavigationBarView == null) {
             mNavigationBarView =
