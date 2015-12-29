3d694d3d11c9663a4217d86c11ef69a7fe2264ac
LorDClockaN <davor@losinj.com>
2015-12-28 20:58:22 -0700

    FWB: Hold back to kill timeout (1/2)
    Change-Id: I90bbe8f50e410a2c9f5427055c108a8af9e82ebd
    Signed-off-by: LorDClockaN <davor@losinj.com>
    
    Conflicts:
    	services/core/java/com/android/server/policy/PhoneWindowManager.java

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -965,6 +965,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.PA_PIE_STATE), false, this,
                     UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.HOLD_BACK_TO_KILL_TIMEOUT), false, this,
             updateSettings();
         }
 
@@ -1825,8 +1827,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 com.android.internal.R.integer.config_deviceHardwareKeys);
         mHasRemovableLid = mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_hasRemovableLid);
-        mBackKillTimeout = mContext.getResources().getInteger(
-                com.android.internal.R.integer.config_backKillTimeout);
 
         updateKeyAssignments();
 
@@ -2426,6 +2426,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             if (mImmersiveModeConfirmation != null) {
                 mImmersiveModeConfirmation.loadSetting(mCurrentUserId);
             }
+
+            mBackKillTimeout = Settings.System.getInt(resolver,
+                    Settings.System.HOLD_BACK_TO_KILL_TIMEOUT, 750);
         }
         synchronized (mWindowManagerFuncs.getWindowManagerLock()) {
             WindowManagerPolicyControl.reloadFromSetting(mContext);
