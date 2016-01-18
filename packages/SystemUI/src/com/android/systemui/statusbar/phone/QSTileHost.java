3696e5192cf0c97465100f23f317e00a98afe79e
Michael Bestas <mikeioannina@gmail.com>
2016-01-17 21:01:23 -0700

    SystemUI: Add heads up tile (1/2)
    Change-Id: I0e3cff73752c59d804dcec43b86524c2e8df4f57
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -52,6 +52,7 @@ import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.EditTile;
 import com.android.systemui.qs.tiles.ExpandedDesktopTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
+import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LiveDisplayTile;
@@ -405,6 +406,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
 	    else if (tileSpec.equals("appsidebar")) return new AppsidebarTile(this);
 	    else if (tileSpec.equals("restartui")) return new SystemUIRestartTile(this);
         else if (tileSpec.equals("lte")) return new LteTile(this);
+        else if (tileSpec.equals("heads_up")) return new HeadsUpTile(this);
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
     }
@@ -506,6 +508,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
 	    else if (spec.equals("appsidebar")) return R.string.quick_settings_app_sidebar;
 	    else if (spec.equals("restartui")) return R.string.quick_settings_systemui_restart_label;
         else if (spec.equals("lte")) return R.string.qs_lte_label;
+        else if (spec.equals("heads_up")) return R.string.quick_settings_heads_up_label;
         return 0;
     }
 
@@ -548,6 +551,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
 	    else if (spec.equals("appsidebar")) return R.drawable.ic_qs_appsidebar_on;
 	    else if (spec.equals("restartui")) return R.drawable.ic_qs_systemui_restart;
         else if (spec.equals("lte")) return R.drawable.ic_qs_lte_on;
+        else if (spec.equals("heads_up")) return R.drawable.ic_qs_heads_up_on;
         return 0;
     }
 
