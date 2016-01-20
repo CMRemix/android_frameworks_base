08fbb1c950bce8b35d722a64d1de0f4481630d4e
LorDClockaN <davor@losinj.com>
2016-01-20 23:23:47 +0100

    FWB: QS header transparency (1/2)
    Change-Id: Ibe86f4405433b46c5e36c4e4d0fad788a5e65724

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4856,6 +4856,12 @@ public final class Settings {
         public static final String QS_TRANSPARENT_HEADER = "qs_transparent_header";
 
         /**
+         * Transparent qs header
+         * @hide
+         */
+        public static final String QS_TRANSPARENT_HEADER = "qs_transparent_header";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
