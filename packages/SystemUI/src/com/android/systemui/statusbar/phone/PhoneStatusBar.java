f20c8bdcdc82623f6a9cf228565583fa4b4d422a
Roman Birg <roman@cyngn.com>
2016-01-26 09:11:19 -0800

    SystemUI: disable editing tiles while in lock screen
    Disable tiles from entering the edit state while keygaurd is up for
    security reasons.
    
    Ref: CYNGNOS-1644
    
    Change-Id: Ic2b7685a6625ef530129a7e3d740a69b89398b6a
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1486,6 +1486,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
                 @Override
                 public void setEditing(final boolean editing) {
+                    if (mState != StatusBarState.SHADE) {
+                        return;
+                    }
                     mHandler.post(new Runnable() {
                         @Override
                         public void run() {
@@ -1502,6 +1505,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
                 @Override
                 public void goToSettingsPage() {
+                    if (mState != StatusBarState.SHADE) {
+                        return;
+                    }
                     setEditing(true);
                     mHandler.postDelayed(new Runnable() {
                         @Override
