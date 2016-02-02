2e33de1daaf1a66c356ac12235e358af80256eea
Carlo Savignano <carlosavignano@aospa.co>
2016-02-02 17:08:12 +0100

    SystemUI: Allow clearing recents tasks within recents button
    * Forward port and simplify code logic for lollipop.
    * Update recents button properly every time setNavigationIconHints() is
      called. Remove the callback that is no longer really needed.
    * Tasks will be cleared within long-pressing recents. It will not clear
      tasks when accessibility mode and/or screen pinning is in use, long-press
      event will be treathed then as a normal recent to allow unlocking
      normally screen-pinning.
    * Defaulted to clearing all of them, on the spot will ask to keep last task.
    * Drawables by arzbhatia
    * Thanks to TheMrcool212 for the initial port patch and for adapting
      cm methods used to dismiss tasks, ignoring last app as for kk.
    
    * Reworked drawables
    * Fixed recents button not updating correctly
    * Ported to CyanogenMod 13
    
    AICPfy:
    Fix clearing tasks on AICP
    
    Signed-off-by: BigBrother1984 <carlosavignano@aospa.co>
    Change-Id: I0a793fc8a174d956ff9b4b9c5d3259897c439795
    Signed-off-by: lion0738 <lion0738@naver.com>
    
    Conflicts:
    
    	packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -105,6 +105,7 @@ public class NavigationBarView extends LinearLayout {
     private Drawable mRecentIcon;
     private Drawable mRecentLandIcon;
     private Drawable mHomeIcon, mHomeLandIcon;
+    private Drawable mRecentAltIcon, mRecentAltLandIcon;
 
     private boolean mDimNavButtons;
     private int mDimNavButtonsTimeout;
@@ -453,6 +454,8 @@ public class NavigationBarView extends LinearLayout {
         mRecentLandIcon = mRecentIcon;
         mHomeIcon = res.getDrawable(R.drawable.ic_sysbar_home);
         mHomeLandIcon = mHomeIcon;
+        mRecentAltIcon = res.getDrawable(R.drawable.ic_sysbar_recent_clear);
+        mRecentAltLandIcon = mRecentAltIcon;
     }
 
     public void updateResources(Resources res) {
@@ -523,7 +526,10 @@ public class NavigationBarView extends LinearLayout {
                 ? (mVertical ? mBackAltLandIcon : mBackAltIcon)
                 : (mVertical ? mBackLandIcon : mBackIcon));
 
-        ((ImageView)getRecentsButton()).setImageDrawable(mVertical ? mRecentLandIcon : mRecentIcon);
+        ((ImageView)getRecentsButton()).setImageDrawable(
+                    (0 != (hints & StatusBarManager.NAVIGATION_HINT_RECENT_ALT))
+                            ? (mVertical ? mRecentAltLandIcon : mRecentAltIcon)
+                            : (mVertical ? mRecentLandIcon : mRecentIcon));
         ((ImageView)getHomeButton()).setImageDrawable(mVertical ? mHomeLandIcon : mHomeIcon);
 
         final boolean showImeButton = ((hints & StatusBarManager.NAVIGATION_HINT_IME_SHOWN) != 0)
@@ -573,6 +579,10 @@ public class NavigationBarView extends LinearLayout {
 
     private void setSideButtonVisibility(boolean left, int vis) {
         mSideButtonVisibilities[mVertical ? 1 : 0][left ? 0 : 1] = vis;
+    }
+
+    public int getNavigationIconHints() {
+        return mNavigationIconHints;
     }
 
     public void setDisabledFlags(int disabledFlags) {
