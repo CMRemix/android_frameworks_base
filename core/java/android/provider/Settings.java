59de220702207af01ee204f4e093948b8a6769a8
LorDClockaN <davor@losinj.com>
2016-02-07 01:55:39 -0700

    FWB: Carrier label font style (1/2)
    Statusbar and keyguard
    
    Change-Id: Ie4f107be3488c621e290c1915561656685bb199c
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4079,6 +4079,12 @@ public final class Settings {
         public static final String STATUS_BAR_CARRIER_FONT_SIZE = "status_bar_carrier_font_size";
 
         /**
+         * Custom carrier label font style
+         * @hide
+         */
+        public static final String STATUS_BAR_CARRIER_FONT_STYLE = "status_bar_carrier_font_style";
+
+        /**
          * Custom carrier label placement
          * @hide
          */
