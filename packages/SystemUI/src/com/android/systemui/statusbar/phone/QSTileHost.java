d428cd8ec71b82cd49e54ac1fbb488f0f6f23310
LorDClockaN <davor@losinj.com>
2016-02-09 02:11:32 -0700

    FWB: App picker tile (1/2)
    Reused navbar icon and string
    
    Change-Id: Ic7a8729749ecdcbbe4e7b5744d64b758645bb34a
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -40,6 +40,7 @@ import com.android.systemui.qs.QSTile;
 import com.android.systemui.qs.tiles.AdbOverNetworkTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
 import com.android.systemui.qs.tiles.AmbientDisplayTile;
+import com.android.systemui.qs.tiles.AppPickerTile;
 import com.android.systemui.qs.tiles.BatterySaverTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
 import com.android.systemui.qs.tiles.BrightnessTile;
@@ -415,6 +416,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
 	    else if (tileSpec.equals("selinux")) return new SElinuxTile(this);
 	    else if (tileSpec.equals("themes")) return new ThemesTile(this);
 	    else if (tileSpec.equals("gesture_anywhere")) return new GestureAnyWhereTile(this);
+        else if (tileSpec.equals("app_picker")) return new AppPickerTile(this);
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
     }
@@ -525,6 +527,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (spec.equals("power_menu")) return R.string.quick_settings_power_menu_label;
         else if (spec.equals("themes")) return R.string.quick_settings_themes;
         else if (spec.equals("gesture_anywhere")) return R.string.quick_settings_gesture_anywhere_label;
+        else if (spec.equals("app_picker")) return R.string.navbar_app_picker;
         return 0;
     }
 
@@ -573,6 +576,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
 	    else if (spec.equals("selinux")) return R.drawable.ic_qs_selinux_enforcing;
 	    else if (spec.equals("themes")) return R.drawable.ic_qs_themes;
 	    else if (spec.equals("gesture_anywhere")) return R.drawable.ic_qs_gestures_on;
+        else if (spec.equals("app_picker")) return R.drawable.ic_sysbar_app_picker;
         return 0;
     }
 
