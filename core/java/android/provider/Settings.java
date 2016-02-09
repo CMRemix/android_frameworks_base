67926feeb441e9b7e050f8884cb5f0e32755ebd7
LorDClockaN <davor@losinj.com>
2016-02-08 23:18:57 -0700

    FWB: Status bar header font style (1/2)
    Change-Id: I79d18c889858919b13aa98c3dc8e56a0145f9548
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4918,6 +4918,12 @@ public final class Settings {
         public static final String STATUS_BAR_WEATHER_ICON_PACK = "status_bar_weather_icon_pack";
 
         /**
+         * Custom status bar header font style
+         * @hide
+         */
+        public static final String STATUS_BAR_HEADER_FONT_STYLE = "status_bar_header_font_style";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
