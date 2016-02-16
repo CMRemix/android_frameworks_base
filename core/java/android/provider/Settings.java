3be8964d05b10a121eb008cba0456321174ad6c2
ZION959 <ziontran@gmail.com>
2016-02-16 00:51:02 -0700

    revert: SystemUI: Add option for "modern" custom headers (1/2)
    Signed-off-by: Janson Kang <temasek71@gmail.com> (reverted from commit b4f603b9745808ca44fd2a6cfa58ca6ce592e890)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3864,8 +3864,7 @@ public final class Settings {
         /**
          * Whether to use default custom header or poly hd
          * 0 = default
-         * 1 = modern
-         * 2 = poly hd
+         * 1 = poly hd
          * @hide
          */
         public static final String STATUS_BAR_CUSTOM_HEADER_DEFAULT = "status_bar_custom_header_default";
