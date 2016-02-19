9e8a6637fe9b422f92031a4c9f88e90cff71bc4d
UtkarshGupta <utkarsh.eminem@gmail.com>
2016-02-19 00:01:02 -0700

    FWB: Battery light: 100% charged level (1/2)
    Conflicts:
    	core/res/res/values/cmremix_symbols.xml
    	core/res/res/values/custom_config.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5188,6 +5188,12 @@ public final class Settings {
         public static final String STATUS_BAR_CUSTOM_HEADER_SHADOW = "status_bar_custom_header_shadow";
 
         /**
+         * What color to use for the battery LED while charging - really full (100%)
+         * @hide
+         */
+        public static final String BATTERY_LIGHT_REALLY_FULL_COLOR = "battery_light_really_full_color";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
