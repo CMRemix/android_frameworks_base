8955c05d1a1d11b9fa42abbb50e9edb18a8e2056
Roman Birg <roman@cyngn.com>
2016-02-18 23:42:40 -0700

    SystemUI: handle camera launch gesture from keyhadler
    Includes partial reverts:
    
        Revert "KeyHandler: don't break old interfaces"
        This reverts commit 31ecbdc5fcc1263e9028107858565d951798fd93.
    
        Revert "framework: move device key handler logic, fix gesture camera launch"
        This reverts commit db1b4e6e4880be1828342be9bc468d36565a2ef0.
    
    Ref: OPO-427
    
    Change-Id: Ic15543a8f63ad4521ab0ab536d00224b0bd70f8c
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1694,6 +1694,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         filter.addAction(Intent.ACTION_SCREEN_OFF);
         filter.addAction(Intent.ACTION_SCREEN_ON);
         filter.addAction(Intent.ACTION_KEYGUARD_WALLPAPER_CHANGED);
+        filter.addAction(cyanogenmod.content.Intent.ACTION_SCREEN_CAMERA_GESTURE);
         context.registerReceiverAsUser(mBroadcastReceiver, UserHandle.ALL, filter, null, null);
 
         IntentFilter demoFilter = new IntentFilter();
@@ -4391,6 +4392,17 @@ private final View.OnClickListener mKillClickListener = new View.OnClickListener
                 mKeyguardWallpaper = wm.getKeyguardBitmap();
                 updateMediaMetaData(true);
                 notifyHeaderViewScreenOn(true);
+            } else if (cyanogenmod.content.Intent.ACTION_SCREEN_CAMERA_GESTURE.equals(action)) {
+                boolean userSetupComplete = Settings.Secure.getInt(mContext.getContentResolver(),
+                        Settings.Secure.USER_SETUP_COMPLETE, 0) != 0;
+                if (!userSetupComplete) {
+                    if (DEBUG) Log.d(TAG, String.format(
+                            "userSetupComplete = %s, ignoring camera launch gesture.",
+                            userSetupComplete));
+                    return;
+                }
+
+                onCameraLaunchGestureDetected(StatusBarManager.CAMERA_LAUNCH_SOURCE_SCREEN_GESTURE);
             }
         }
     };
