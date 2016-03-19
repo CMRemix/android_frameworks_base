b9ad66509a29736d28a9d71c2efd8c3034605ffb
ZION959 <ziontran@gmail.com>
2016-03-19 02:33:04 -0700

    revert: Add 3Minit battery mod
    Change-Id: I42cdd5733959919de00f48621937cf2da692bda8
    
    Conflicts:
    	packages/SystemUI/res/layout/system_icons.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java (reverted from commit 7cf60879d71e29d2d5978fe84823adeb653226fc)

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -46,7 +46,6 @@ import com.android.systemui.statusbar.SignalClusterView;
 import com.android.systemui.statusbar.policy.BatteryController;
 import com.android.systemui.statusbar.policy.DockBatteryController;
 import com.android.systemui.statusbar.policy.KeyguardUserSwitcher;
-import com.android.systemui.statusbar.policy.MinitBattery;
 import com.android.systemui.statusbar.policy.UserInfoController;
 import com.android.systemui.statusbar.policy.UserSwitcherController;
 
@@ -134,11 +133,6 @@ public class KeyguardStatusBarView extends RelativeLayout {
     protected void onFinishInflate() {
         super.onFinishInflate();
         mSystemIconsSuperContainer = findViewById(R.id.system_icons_super_container);
-        MinitBattery mb = (MinitBattery) mSystemIconsSuperContainer.findViewById(R.id.minitBattery);
-        if(!mb.isSetup()){
-           BatteryMeterView bmv = (BatteryMeterView) mSystemIconsSuperContainer.findViewById(R.id.battery);
-           bmv.setVisibility(View.VISIBLE);
-        }
         mSignalCluster = (SignalClusterView) findViewById(R.id.signal_cluster);
         mMultiUserSwitch = (MultiUserSwitch) findViewById(R.id.multi_user_switch);
         mMultiUserAvatar = (ImageView) findViewById(R.id.multi_user_avatar);
