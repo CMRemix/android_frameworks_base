bb6dab5dfebd968b1651ad7ea296e67d5c11c0dd
Andrzej Ressel <jereksel@gmail.com>
2016-03-21 16:25:07 -0700

    FWB: Themes Tile (1/2) Preview: https://youtu.be/s3RHi_DSn10
    AICPfy
    - Make it work with AICP source:
      - ThemesTile: use CMMetricsLogging,
        override needed method for CustomTile
        and add needed import
      - Adapt to CM's tile customizations
    - IF YOU ARE GONNA PICK THIS, AT LEAST DON'T REMOVE
    AICPFY STUFF, JUST ADD YOUR CHANGES BELOW IT
    
    Change-Id: I081c1253de4df0a95580c43e9a64944a58c2334e
    
    Conflicts:
    	packages/SystemUI/AndroidManifest.xml
    	packages/SystemUI/res/values/cmremix_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -84,6 +84,7 @@ import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.ScreenTimeoutTile;
 import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.SyncTile;
+import com.android.systemui.qs.tiles.ThemesTile;
 import com.android.systemui.qs.tiles.UsbTetherTile;
 import com.android.systemui.qs.tiles.VolumeTile;
 import com.android.systemui.qs.tiles.WifiTile;
@@ -421,8 +422,9 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("app_picker")) return new AppPickerTile(this);
 	    else if (tileSpec.equals("kill_app")) return new KillAppTile(this);
         else if (tileSpec.equals("caffeine")) return new CaffeineTile(this);
-   	    else if (tileSpec.equals("hw_keys")) return new HardwareKeysTile(this);	
+   	    else if (tileSpec.equals("hw_keys")) return new HardwareKeysTile(this);
         else if (tileSpec.equals("sound")) return new SoundTile(this);
+        else if (tileSpec.equals("themes")) return new ThemesTile(this);
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
     }
@@ -537,6 +539,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (spec.equals("caffeine")) return R.string.quick_settings_caffeine_label;
         else if (spec.equals("hw_keys")) return R.string.quick_settings_hwkeys_title;
         else if (spec.equals("sound")) return R.string.quick_settings_sound_label;
+        else if (spec.equals("themes")) return R.string.quick_settings_themes;
         return 0;
     }
 
@@ -587,8 +590,9 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (spec.equals("app_picker")) return R.drawable.ic_sysbar_app_picker;
  	    else if (spec.equals("kill_app")) return R.drawable.ic_app_kill;
         else if (spec.equals("caffeine")) return R.drawable.ic_qs_caffeine_on;
-	else if (spec.equals("hw_keys")) return R.drawable.ic_qs_hwkeys_on;
+        else if (spec.equals("hw_keys")) return R.drawable.ic_qs_hwkeys_on;
         else if (spec.equals("sound")) return R.drawable.ic_qs_ringer_audible;
+        else if (spec.equals("themes")) return R.drawable.ic_qs_themes_on;
         return 0;
     }
 
