bc923e8e10fcac68116f31f17bfd76fcc3bbc27c
Danesh M <daneshm90@gmail.com>
2016-04-05 10:39:46 -0700

    Lockscreen : Fix bottom area visibility
    NIGHTLIES-2754
    CYNGNOS-2349
    
    Change-Id: I93ebd95fb15effed83659fadb92f76bf37b39f37

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
@@ -138,6 +138,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
     private boolean mBottomAreaAttached;
     private final WindowManager.LayoutParams mWindowLayoutParams;
     private OnInterceptTouchEventListener mInterceptTouchListener;
+    private BroadcastReceiver mDevicePolicyReceiver;
 
     private final ServiceConnection mPrewarmConnection = new ServiceConnection() {
 
@@ -188,7 +189,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
     private void removeKeyguardBottomArea() {
         if (mBottomAreaAttached) {
             try {
-                mWindowManager.removeViewImmediate(this);
+                mWindowManager.removeView(this);
             } catch (IllegalArgumentException e) {
                 Log.e(TAG, e.getMessage());
             }
@@ -547,6 +548,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
     private void watchForCameraPolicyChanges() {
         final IntentFilter filter = new IntentFilter();
         filter.addAction(DevicePolicyManager.ACTION_DEVICE_POLICY_MANAGER_STATE_CHANGED);
+        mDevicePolicyReceiver = new DevicePolicyBroadcastReceiver();
         getContext().registerReceiverAsUser(mDevicePolicyReceiver,
                 UserHandle.ALL, filter, null, null);
         KeyguardUpdateMonitor.getInstance(mContext).registerCallback(mUpdateMonitorCallback);
@@ -891,7 +893,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
 		updateIndicationTextColor();
     }
 
-    private final BroadcastReceiver mDevicePolicyReceiver = new BroadcastReceiver() {
+    private final class DevicePolicyBroadcastReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
             post(new Runnable() {
@@ -1091,7 +1093,10 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
         mAccessibilityController.removeStateChangedCallback(this);
-        mContext.unregisterReceiver(mDevicePolicyReceiver);
+        if (mDevicePolicyReceiver != null) {
+            mContext.unregisterReceiver(mDevicePolicyReceiver);
+            mDevicePolicyReceiver = null;
+        }
         mShortcutHelper.cleanup();
         mUnlockMethodCache.removeListener(this);
     }
