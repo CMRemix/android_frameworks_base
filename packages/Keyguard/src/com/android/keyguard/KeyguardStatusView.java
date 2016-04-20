3589ae9a4bf645acbbddcff61548ca6f600987d4
ZION959 <ziontran@gmail.com>
2016-04-20 04:04:42 -0700

    revert: Don't refresh ui when screen off
    KeyguardStatusView is doing refresh all the time,
    which cause high power when screen off
    
    PatchSet:
    - We turn of the refresh when screen is off which is a good
      idea. But we should force latest next refresh when screen turns
      on. Du that a timechanged refresh maybe comes a bit later.
    
    Change-Id: Icbb9a92bae3cf099417ab820617dd3f17e49f75a (reverted from commit 810c77dc46b5c44f67df703c65cc258d092687e3)

--- packages/Keyguard/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardStatusView.java
@@ -74,20 +74,12 @@ public class KeyguardStatusView extends GridLayout implements
     private int mIconNameValue = 0;
     private WeatherController mWeatherController;
 
-    //On the first boot, keygard will start to receiver TIME_TICK intent.
-    //And onScreenTurnedOff will not get called if power off when keyguard is not started.
-    //Set initial value to false to skip the above case.
-    private boolean mEnableRefresh = false;
-
     private KeyguardUpdateMonitorCallback mInfoCallback = new KeyguardUpdateMonitorCallback() {
 
         @Override
         public void onTimeChanged() {
-            if (mEnableRefresh) {
-                refresh();
                 updateClockColor();
                 updateClockDateColor();
-            }
         }
 
         @Override
@@ -104,8 +96,6 @@ public class KeyguardStatusView extends GridLayout implements
         @Override
         public void onStartedWakingUp() {
             setEnableMarquee(true);
-            mEnableRefresh = true;
-            refresh();
             updateClockColor();
             updateClockDateColor();
         }
@@ -113,7 +103,6 @@ public class KeyguardStatusView extends GridLayout implements
         @Override
         public void onFinishedGoingToSleep(int why) {
             setEnableMarquee(false);
-            mEnableRefresh = false;
         }
 
         @Override
