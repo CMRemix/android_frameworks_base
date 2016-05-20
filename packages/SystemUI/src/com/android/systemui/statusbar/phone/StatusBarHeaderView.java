af8189804412f619b4e9906f693a5e89319eb74a
Roman Birg <roman@cyngn.com>
2016-05-19 21:27:42 -0700

    SystemUI: update qs tile hint and header view on locale change
    Ticket: CYNGNOS-2814
    
    Change-Id: I305ac5eca862ffa5c525ec6b39bfeddf03719227
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/qs/QSDragPanel.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -467,9 +467,15 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
         FontSizeUtils.updateFontSize(this, R.id.empty_time_view, R.dimen.qs_time_expanded_size);
 
         mEmergencyCallsOnly.setText(com.android.internal.R.string.emergency_calls_only);
+
         mClockCollapsedSize = getResources().getDimensionPixelSize(R.dimen.qs_time_collapsed_size);
         mClockExpandedSize = getResources().getDimensionPixelSize(R.dimen.qs_time_expanded_size);
         mClockCollapsedScaleFactor = (float) mClockCollapsedSize / (float) mClockExpandedSize;
+
+        if (mEditTileDoneText != null) {
+            mEditTileDoneText.setText(R.string.quick_settings_done);
+        }
+
         updateClockScale();
         updateClockCollapsedMargin();
 	setclockcolor();
