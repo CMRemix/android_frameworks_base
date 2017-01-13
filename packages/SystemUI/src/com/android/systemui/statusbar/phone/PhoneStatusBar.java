1c7aeec7fd10da3539d4c007db60de362a7757a2
0xD34D <clark@scheffsblend.com>
2017-01-12 20:55:48 -0700

    SystemUI: Lockscreen shortcut customization
    Change-Id: I4abf1deed0a342e111453516fa7820191a55ffc4
    
    Revert "SystemUI: recalculate clock position on removing empty shade views"
    
    This reverts commit 023d3365524584de91acf4716822553f38aa507b.
    
    Change-Id: I79bde45cd57e2523dbfa3211c5971c4b82a240c0
    
    Lockscreen : Only animate targets if launched via swipe
    
    If launching via gesture or double tap of power button, showing the
    reveal doesn't make much sense and creates visual jank. Only animate
    for swipes.
    
    Change-Id: I69e1d06c77c12fe39b5ddb01bcc22ed2fff2a9d0
    
    Keyguard : Ensure we expand onIconClicked
    
    Not doing so shows visible clipping when the notification
    panel is expanded mainly due to the limited height of the bar.
    
    Change-Id: I50a35b186b3efb8a0e4ba0ae18ff915aebe236c6
    
    SystemUI: fix double tap power launching custom lockscreen icon
    
    Ticket: RM-232
    
    Change-Id: I2ddf9fce55e9deb9cdf70416f0ed2ee5ce6ce225
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Fix leftAffordanceView visibility bug
    
    When you disable the left bottom lockscreen target, is suppose to be completely disabled like the right target.
    While the icon itself disappears, the preview and hint are still present. This fixes that :-)
    
    See images to see bug:
    http://i.imgur.com/WhBvZkc.jpg & http://i.imgur.com/otyOn6m.png
    
    Change-Id: If5b30f03649d5081409a98dbd36c49b99a5095d7
    
    Ensure camera is launched if doing screen off gesture
    
    Currently it launches whatever the right affordance is set to, which is
    incorrect. Ensure screen off gesture always launches camera.
    
    Change-Id: I251d37ccc85d014ff46714b222b9074b2092f6e1
    
    SystemUI : Ensure previous camera preview is removed
    
    For lockscreen shortcuts, ensure if the camera intent changes,
    we remove the previous view.
    
    @xanaxdroid added DelimitedString parts to Settings Provider
    
    Change-Id: I7c44353d25ddc47c3d986acf32dd9a3e85915c82
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -164,6 +164,7 @@ import com.android.systemui.stackdivider.Divider;
 import com.android.systemui.stackdivider.WindowManagerProxy;
 import com.android.systemui.statusbar.ActivatableNotificationView;
 import com.android.systemui.statusbar.BackDropView;
+import com.android.systemui.statusbar.AppSidebar;
 import com.android.systemui.statusbar.BaseStatusBar;
 import com.android.systemui.statusbar.CommandQueue;
 import com.android.systemui.statusbar.DismissView;
@@ -503,6 +504,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     false, this, UserHandle.USER_ALL);
     		resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.SHOW_THREEG),
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.APP_SIDEBAR_POSITION),
                     false, this, UserHandle.USER_ALL);
             update();
         }
@@ -1009,6 +1012,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
 
         addGestureAnywhereView();
+        addSidebarView();
 
         mAssistManager = SystemUIFactory.getInstance().createAssistManager(this, context);
 
@@ -1217,6 +1221,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         // receive broadcasts
         IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_CONFIGURATION_CHANGED);
         filter.addAction(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
         filter.addAction(Intent.ACTION_SCREEN_OFF);
         filter.addAction(Intent.ACTION_SCREEN_ON);
@@ -3975,6 +3980,24 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 finishBarAnimations();
                 resetUserExpandedStates();
             }
+            else if (Intent.ACTION_CONFIGURATION_CHANGED.equals(action)) {
+                Configuration config = mContext.getResources().getConfiguration();
+                try {
+                    // position app sidebar on left if in landscape orientation and device has a navbar
+                    if (mWindowManagerService.hasNavigationBar() &&
+                            config.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                        mWindowManager.updateViewLayout(mAppSidebar,
+                                getAppSidebarLayoutParams(AppSidebar.SIDEBAR_POSITION_LEFT));
+                        mHandler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                mAppSidebar.setPosition(AppSidebar.SIDEBAR_POSITION_LEFT);
+                            }
+                        }, 500);
+                    }
+                } catch (RemoteException e) {
+                }
+            }
             else if (Intent.ACTION_SCREEN_ON.equals(action)) {
                 mScreenOn = true;
                 notifyNavigationBarScreenOn(true);
@@ -4099,6 +4122,17 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
         if (mSecurityController != null) {
             mSecurityController.onUserSwitched(mCurrentUserId);
+        }
+    }
+
+    private void updateSettings() {
+        ContentResolver resolver = mContext.getContentResolver();
+        int sidebarPosition = Settings.System.getInt(resolver,
+                Settings.System.APP_SIDEBAR_POSITION, 
+                AppSidebar.SIDEBAR_POSITION_LEFT);
+        if (sidebarPosition != mSidebarPosition) {
+            mSidebarPosition = sidebarPosition;
+            mWindowManager.updateViewLayout(mAppSidebar, getAppSidebarLayoutParams(sidebarPosition));
         }
     }
 
