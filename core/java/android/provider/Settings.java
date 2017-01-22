12c17e8b22fbcd0263c0717250147042cdecbd40
Alex Cruz <mazdarider23@gmail.com>
2017-01-22 11:42:00 +0300

    Rework dashboard settings [2/2]
    Change-Id: I91221badb75ebd755479ee0de8f0e88026109e6d
    
    # Conflicts:
    #	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3962,20 +3962,8 @@ public final class Settings {
          */
         public static final String IMMERSIVE_RECENTS = "immersive_recents";
 
-        /**
-         * Disable dashboard suggestions in settings
-         * @hide
-         */
-        public static final String DISABLE_SUGGESTIONS = "disable_suggestions";
 
-        /**
-         * Remove tile summaries in settings
-         * @hide
-         */
-        public static final String REMOVE_TILE_SUMMARY = "remove_tile_summary";
-
-
-		/**
+       /**
          * Whether to use gesture anywhere feature.
          * @hide
          */
@@ -4669,12 +4657,6 @@ public final class Settings {
         public static final String DISABLE_SUGGESTIONS = "disable_dashboard_suggestions";
 
         /**
-         * Whether to show dashboard summaries in a single line or not
-         * @hide
-         */
-        public static final String DASHBOARD_SUMMARY_DOUBLE_LINES = "dashboard_summary_double_lines";
-
-        /**
          * Remove tile summaries in settings
          * @hide
          */
@@ -4687,10 +4669,16 @@ public final class Settings {
         public static final String DISABLE_DASHBOARD_CONDITIONS = "disable_dashboard_conditions";
 
         /**
-         * Dashboard columns in settings
+         * Dashboard portrait columns in settings
          * @hide
          */
-        public static final String DASHBOARD_COLUMNS = "dashboard_columns";
+        public static final String DASHBOARD_PORTRAIT_COLUMNS = "dashboard_portrait_columns";
+
+        /**
+         * Dashboard landscape columns in settings
+         * @hide
+         */
+        public static final String DASHBOARD_LANDSCAPE_COLUMNS = "dashboard_landscape_columns";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
