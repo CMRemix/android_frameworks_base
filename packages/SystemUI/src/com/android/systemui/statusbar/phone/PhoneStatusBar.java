3300ebd994a971b16ea09f3ed519d463bb91c921
SpiritCroc <spiritcroc@gmail.com>
2017-03-19 16:34:18 +0100

    Statusbar ticker: apply statusbar tint when set to white
    Increase readability when using inversed statusbar
    
    Change-Id: I7f6f657f9130be013945c222f1133a5d48d8c8e8
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1980,6 +1980,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
     }
 
+    void setTickerTint(int color) {
+        if (mTicker != null) {
+            mTicker.setDefaultColor(color);
+        }
+    }
+
     public int getStatusBarHeight() {
         if (mNaturalBarHeight < 0) {
             final Resources res = mContext.getResources();
