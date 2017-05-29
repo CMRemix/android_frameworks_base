b7183d7f5441673b94c21e9c476a6684cb80d70a
ZION959 <minhdtran43@gmail.com>
2017-05-29 15:37:40 -0700

    OMS7-N: fixed build
    Change-Id: Ic524595dac03f008e61b594ae4ee8cce9d6b04d5

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -2350,8 +2350,8 @@ public class PackageManagerService extends IPackageManager.Stub {
             if (!overlayThemeDir.isEmpty()) {
                 scanDirTracedLI(new File(VENDOR_OVERLAY_DIR, overlayThemeDir), mDefParseFlags
                         | PackageParser.PARSE_IS_SYSTEM
-                        | PackageParser.PARSE_IS_SYSTEM_DIR
-                        | PackageParser.PARSE_TRUSTED_OVERLAY, scanFlags | SCAN_TRUSTED_OVERLAY, 0);
+                        | PackageParser.PARSE_IS_SYSTEM_DIR,
+                        scanFlags, 0); 
             }
             scanDirTracedLI(new File(VENDOR_OVERLAY_DIR), mDefParseFlags
                     | PackageParser.PARSE_IS_SYSTEM
@@ -16547,8 +16547,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                     false /*installed*/, true /*stopped*/, true /*notLaunched*/,
                     false /*hidden*/, false /*suspended*/, null, null, null,
                     false /*blockUninstall*/,
-                    ps.readUserState(nextUserId).domainVerificationStatus, 0,
-                    null, null);
+                    ps.readUserState(nextUserId).domainVerificationStatus, 0,null, null, null);
         }
     }
 
