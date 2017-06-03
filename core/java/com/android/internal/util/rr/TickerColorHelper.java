AICP/frameworks_base

FWB: The Return of the Ticker 2/2
Return of ticker work done by @rogersb11
Included are on and off switch from @Jubakuba

Custom color selection for Ticker text and icons also font and size

AICPfy:
- Bring it to AICP source
- No need to update API

Change-Id: I6590759af0bdda9bba4f60e9c3f768b065b856b7

core/java/com/android/internal/util/aicp/TickerColorHelper.java
@@ -1,0 +1,42 @@
+/*
+* Copyright (C) 2015 DarkKat
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.aicp;
+
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.provider.Settings;
+
+public class TickerColorHelper {
+
+    private static final int WHITE = 0xffffffff;
+
+    public static int getTickerTextColor(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.STATUS_BAR_TICKER_TEXT_COLOR, WHITE);
+    }
+
+    public static ColorStateList getTickerIconColorList(Context context) {
+        return ColorStateList.valueOf(getTickerIconColor(context));
+    }
+
+    public static int getTickerIconColor(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.STATUS_BAR_TICKER_ICON_COLOR,
+                WHITE);
+    }
+}

CMRemix/android_frameworks_base

Signed-off-by: Varun Date <date.varun123@gmail.com>

core/java/com/android/internal/util/rr/TickerColorHelper.java
@@ -1,0 +1,42 @@
+/*
+* Copyright (C) 2015 DarkKat
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.ee;
+
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.provider.Settings;
+
+public class TickerColorHelper {
+
+    private static final int WHITE = 0xffffffff;
+
+    public static int getTickerTextColor(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.STATUS_BAR_TICKER_TEXT_COLOR, WHITE);
+    }
+
+    public static ColorStateList getTickerIconColorList(Context context) {
+        return ColorStateList.valueOf(getTickerIconColor(context));
+    }
+
+    public static int getTickerIconColor(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.STATUS_BAR_TICKER_ICON_COLOR,
+                WHITE);
+    }
+}