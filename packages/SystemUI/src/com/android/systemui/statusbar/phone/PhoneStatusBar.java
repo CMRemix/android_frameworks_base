a5ee6bb09082a332d3a0af46c1c524ddd910953f
ezio84 <brabus84@gmail.com>
2017-06-23 17:45:08 +0530

    Fix System Navigation fp switch
    Missing observer and call
    
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    
    # Conflicts:
    #	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -757,6 +757,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.QS_FOOTER_WARNINGS),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.Secure.getUriFor(
+                  Settings.Secure.SYSTEM_NAVIGATION_KEYS_ENABLED),
+                  false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                   Settings.System.FP_SWIPE_CALL_ACTIONS),
                   false, this, UserHandle.USER_ALL);
@@ -967,6 +970,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mFpSwipeCallActions = Settings.System.getIntForUser(mContext.getContentResolver(),
                     Settings.System.FP_SWIPE_CALL_ACTIONS, 0, UserHandle.USER_CURRENT);
 
+            mSystemNavigationKeysEnabled = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                    Settings.Secure.SYSTEM_NAVIGATION_KEYS_ENABLED, 0, UserHandle.USER_CURRENT) == 1;
+
+                    
             RecentsActivity.updateBlurColors(mBlurDarkColorFilter,mBlurMixedColorFilter,mBlurLightColorFilter);
             RecentsActivity.updateRadiusScale(mScaleRecents,mRadiusRecents);
 
