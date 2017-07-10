7b82efd13d902961cc91b08ff292920d1f4f6614
ZION959 <minhdtran43@gmail.com>
2017-07-10 16:10:30 -0700

    revert: Add gestures control [3/4]
    * Database policy.
    * KeyHandler for gestures inputs.
    * Input policy integration.
    * Supported ALL QCOM chipsets synaptic gestures.
    
    Ticket: NOUGAT-17
    
    Change-Id: I47a61cf054b56d601a4a332359daabb3c666309f
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    Signed-off-by: Willi Ye <williye97@gmail.com> (reverted from commit 464faeb6bfc045a4a73a5d8cc62d15a064b75f72)

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -871,9 +871,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     // Maps global key codes to the components that will handle them.
     private GlobalKeyManager mGlobalKeyManager;
 
-    // Gesture key handler.
-    private KeyHandler mKeyHandler;
-
     // Fallback actions by key code.
     private final SparseArray<KeyCharacterMap.FallbackAction> mFallbackActions =
             new SparseArray<KeyCharacterMap.FallbackAction>();
@@ -2437,12 +2434,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 Slog.w(TAG, "Could not instantiate device key handler "
                         + deviceKeyHandlerLibs[i] + " from class "
                         + deviceKeyHandlerClasses[i], e);
-            }
-
-        boolean enableKeyHandler = context.getResources().
-                getBoolean(com.android.internal.R.bool.config_enableKeyHandler);
-        if (enableKeyHandler) {
-            mKeyHandler = new KeyHandler(mContext);
             }
         }
         if (DEBUG) Slog.d(TAG, "" + mDeviceKeyHandlers.size() + " device key handlers loaded");
@@ -6860,16 +6851,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     + " policyFlags=" + Integer.toHexString(policyFlags));
         }
 
-        /**
-         * Handle gestures input earlier then anything when screen is off.
-         * @author Carlo Savignano
-         */
-        if (!interactive) {
-            if (mKeyHandler != null && mKeyHandler.handleKeyEvent(event)) {
-                return 0;
-            }
-        }
-
         // Basic policy based on interactive state.
         int result;
         boolean isWakeKey = (policyFlags & WindowManagerPolicy.FLAG_WAKE) != 0
@@ -8439,9 +8420,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         mKeyguardDelegate.onBootCompleted();
         mSystemGestures.systemReady();
         mImmersiveModeConfirmation.systemReady();
-        if (mKeyHandler != null) {
-            mKeyHandler.systemReady();
-        }
     }
 
     /** {@inheritDoc} */
